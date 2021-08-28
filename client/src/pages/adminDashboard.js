de7ee72d4a85c99b0f91c70a68c55e482a184578
Dhruvil Pandey <dhruvil7852@gmail.com>
2021-08-28 10:55:06 +0530

    make it better

--- client/src/pages/adminDashboard.js
+++ client/src/pages/adminDashboard.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react'
 import Main from '../components/adminDashboard/main/Main';
 import Sidebar from "../components/adminDashboard/sidebar/Sidebar";
 
 
 const AdminDashboard = () => {
+  let scroll = 0;
+    window.addEventListener('scroll', () => {
+      if(window.location.pathname === '/'){
+          scroll = window.pageYOffset
+          return scroll;
+      }
+  })
+
+  useEffect(() => {
+      setTimeout(() => {
+          window.scrollTo({top: scroll, behavior: 'smooth'})
+      }, 100)
+  },[])
 
   return (
     <div className="container1">
